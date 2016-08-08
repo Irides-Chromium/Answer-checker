9418e77c21811c9a48502f1dc27ddd7a71b699b1
Stephan <zhuhaoqing@live.cn>
2016-08-08 17:02:26 +0800

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+
+raw_my = input("Your Answer: ")
+raw_right = input("Correct Answer: ")
+
+my = ''.join(list(filter(lambda a: a in "ABCDEFGHJ?", raw_my)))
+right = ''.join(list(filter(lambda a: a in "ABCDEFGHJ?", raw_right)))
+
+if len(my) != len(right):
+    print("Length not equal.", file=sys.stderr)
+    sys.exit()
+
+count = 0
+prob_num = len(my)
+
+for i in range(prob_num):
+    if right[i] != my[i]:
+        print("{num: >2}. {right} ({my})".format( \
+                num=i + 1, right=right[i], my=my[i]))
+        count += 1
+
+if count > 0: print("Total/Wrong/Right/Rate: {}".format( \
+            prob_num, count, prob_num - count, count / prob_num))
